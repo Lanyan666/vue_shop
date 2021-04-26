d1138542fd50c254f98d113eadc191ae0bbbe103
lanyan <754847356@qq.com>
2021-04-26 15:47:36 +0800

    新增用户列表界面

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import Users from './components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -9,14 +11,22 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Login },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users }
+      ]
+    }
   ]
 })
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-  if(to.path === '/login') return next()
-  const tokenStr = window.sessionStorage.getItem('token');
-  if(!tokenStr) return next('/login')
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
   next()
 })
 export default router
